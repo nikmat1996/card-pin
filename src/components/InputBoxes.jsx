954fcc70234c297651e51db2a4b45d53ef0cdf59
nikmat1996 <nikhilmathew1996@gmail.com>
2020-09-18 11:46:27 +0530

    automatic switching of inputBoxes done

--- src/components/InputBoxes.jsx
+++ src/components/InputBoxes.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
 
+const handleChange = (e, perBox) => {
+    const { name, value } = e.target;
+    // console.log(name, value, perBox, elemRefs, name)
+    let elem = null;
+    if(value.length === perBox && name !== elemRefs.length - 1) elem = elemRefs[Number(name) + 1]
+    if(value.length === 0      && name !== 0)                   elem = elemRefs[Number(name) - 1]
+    if(elem) elem.current.focus()
+}
+
 const elemRefs = []
 
-const Input = (props) => {
-    const ref = React.createRef();
+const Input = props => {
+    const ref = React.createRef(), { index, perBox, handleChange } = props;
     elemRefs.push(ref);
-    return (
-        <input
-        className="block"
-        name={props.index}
-        ref={ref}
-        onChange={""}
-        placeholder={props.index}
-        />
-    );
+    return  <input name={index} ref={ref} onChange={e => handleChange(e,perBox)} placeholder={index} maxLength={perBox} />
 };
 
-const InputBoxes = ({ dimensions }) => new Array(Number(dimensions.noBoxes)).fill("").map((item, index) => <Input key={index} index={index} handleChange={""} />)
+const InputBoxes = ({ dimensions }) => new Array(Number(dimensions.noBoxes)).fill("").map((item, index) => 
+    <Input key={index} index={index} perBox={Number(dimensions.perBox)} handleChange={handleChange} />)
+
 
 export default InputBoxes
