5b73f634cc881afd590ce4b8ed519d0e66a20e15
nikmat1996 <nikhilmathew1996@gmail.com>
2020-09-18 17:19:15 +0530

    completed copy_pasting pin bug

--- src/components/Dwani.jsx
+++ src/components/Dwani.jsx
@@ -6,29 +6,33 @@ export default class Dwani extends Component {
         super(props)
     
         this.state = {
-            noBoxes: 4,
-            perBox: 4
+            numBoxes: 4,
+            perBox: 4, 
         }
     }
 
-    handleChange = e => {
-        this.setState({ [e.target.name] : e.target.value })
-    }
+    handleChange = e => this.setState({ [e.target.name] : e.target.value ? Number(e.target.value) : e.target.value })
     
     render() {
+        const { numBoxes, perBox } = this.state
         return (
             <>
                 <div className="center">
                     <div className="input_wrapper">
-                        <label htmlFor="noBoxes"> Number of Input Boxes</label>    
-                        <input type="tel" name="noBoxes" defaultValue="4" maxLength="1" onChange={this.handleChange} />
+                        <label htmlFor="numBoxes"> Number of Input Boxes</label>    
+                        <input type="tel" name="numBoxes"  value={numBoxes} maxLength="1" onChange={this.handleChange} />
                     </div>
                     <div className="input_wrapper">
                         <label htmlFor="perBox">Characters Per Box</label>    
-                        <input type="tel" name="perBox" defaultValue="4" maxLength="1" onChange={this.handleChange} />
+                        <input type="tel" name="perBox"  value={perBox} maxLength="1" onChange={this.handleChange} />
                     </div>
                 </div>
-                <InputBoxes dimensions={this.state} />
+                {
+                    numBoxes && perBox ?             
+                    <div className="input_container">
+                        <InputBoxes dimensions={this.state} />
+                    </div> : <div className="input_container"></div>
+                }
             </>
         )   
     }
--- src/components/InputBoxes.jsx
+++ src/components/InputBoxes.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
 
 
-const handleChange = (e, perBox) => {
-    const { name, value } = e.target;
-    // console.log(name, value, perBox, elemRefs, name)
-    let elem = null;
-    if(value.length === perBox && name !== elemRefs.length - 1) elem = elemRefs[Number(name) + 1]
-    if(value.length === 0      && name !== 0)                   elem = elemRefs[Number(name) - 1]
-    if(elem) elem.current.focus()
+const handleChange = (e, dimensions) => {
+    let { name, value } = e.target;
+    const { perBox, numBoxes } = dimensions; 
+    let focusElem = null;
+
+    if(value.length === perBox && name !== elemRefs.length - 1) focusElem = elemRefs[Number(name) + 1]
+    if(value.length === 0      && name !== 0)                   focusElem = elemRefs[Number(name) - 1]
+    
+    if(value.length > perBox){
+        var remainingVal = ""
+        for(let i = name ; i < numBoxes ; i++){
+            remainingVal += elemRefs[i].current.value
+        }
+        
+        let dummy = ""
+        for(let char of remainingVal){
+            dummy += char
+            if(dummy.length === perBox){
+                elemRefs[name].current.value = dummy
+                dummy = ""
+                name++
+            }
+            if(name === numBoxes)  break;
+        }
+        if(dummy) elemRefs[name].current.value = dummy
+        
+        focusElem = elemRefs[name] || elemRefs[name - 1]
+    }
+
+    if(focusElem) focusElem.current.focus()
 }
 
 const elemRefs = []
 
 const Input = props => {
-    const ref = React.createRef(), { index, perBox, handleChange } = props;
+    const ref = React.createRef(), { index, dimensions, handleChange } = props;
+    const { perBox, numBoxes } = dimensions;
+    if(index === 0) elemRefs.length = 0
     elemRefs.push(ref);
-    return  <input name={index} ref={ref} onChange={e => handleChange(e,perBox)} placeholder={index} maxLength={perBox} />
+    return  <input name={index} ref={ref} onChange={ e => handleChange(e, dimensions) } maxLength={elemRefs.length === numBoxes ? perBox : null} />
 };
 
-const InputBoxes = ({ dimensions }) => new Array(Number(dimensions.noBoxes)).fill("").map((item, index) => 
-    <Input key={index} index={index} perBox={Number(dimensions.perBox)} handleChange={handleChange} />)
+const InputBoxes = ({ dimensions }) => new Array(Number(dimensions.numBoxes)).fill("").map((item, index) => 
+    <Input key={index} index={index} dimensions={dimensions} handleChange={handleChange} />)
 
 
 export default InputBoxes
